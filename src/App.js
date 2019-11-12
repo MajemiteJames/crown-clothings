ce3d848e915d89d8cbd224d990560ff09d82e8e2
MajemiteJames <majemiteokoro@gmail.com>
2019-11-12 13:22:30 +0100

    made the homepage to be a component base and added styling to it

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 
-import HomePage from './homepage.component';
+import HomePage from '../src/components/pages/hompage/homepage.component';
 
 function App() {
   return (
