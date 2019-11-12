f1afc297eeab317589faa9b68370b27b2ac239c1
MajemiteJames <majemiteokoro@gmail.com>
2019-11-12 21:19:45 +0100

    installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
 
@@ -7,7 +9,9 @@ import HomePage from '../src/components/pages/hompage/homepage.component';
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
