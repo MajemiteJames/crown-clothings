af4d264384efa6097a6afb8eaafcf08f4ba413d2
MajemiteJames <majemiteokoro@gmail.com>
2019-11-14 18:31:32 +0100

    implemented the sign in page in the sign in and sign up page and styling with a custom button to use for the entire app

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 import HomePage from '../src/components/pages/hompage/homepage.component';
 import ShopPage from './components/pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
