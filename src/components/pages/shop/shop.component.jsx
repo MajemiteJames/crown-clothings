9ed3e185627ec3fda69b6cf19028d43618cf8744
MajemiteJames <majemiteokoro@gmail.com>
2020-01-10 13:16:01 +0100

    implemented a nested routing system from the landing page to the different collections using the find method

--- src/components/pages/shop/shop.component.jsx
+++ src/components/pages/shop/shop.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
 
-const ShopPage = ({ collections }) => (
-            <div>
-               <CollectionsOverview />
+const ShopPage = ({ match }) => (
+            <div className='shop-page'>
+                <Route exact path={`${match.path}`} component={CollectionsOverview} />
+                <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
             </div>
         );
 
