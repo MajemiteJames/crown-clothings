23cd99317f591d73ec6554dc4159b2800e7abef6
MajemiteJames <majemiteokoro@gmail.com>
2020-01-14 10:21:12 +0100

    impelemeted stripe button component and also updated the stripe integration and favicon, title

--- src/components/pages/checkout/checkout.component.jsx
+++ src/components/pages/checkout/checkout.component.jsx
@@ -3,7 +3,8 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import { selectCartItems, selectCartTotal } from '../../../redux/cart/cart.selector';
-import CheckoutItem from '../../checkout-item/checkout-item.component'
+import CheckoutItem from '../../checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../stripe-button/stripe-button.component'
 
 import './checkout.styles.scss';
 
@@ -35,6 +36,14 @@ const CheckoutPage = ({cartItems, total}) => (
         <div className="total">
             <span>TOTAL: ${total}</span>
         </div>
+
+        <div className="test-warning">
+            *Please use the following test credit card for payment*
+            <br />
+            4242 4242 4242 4242 -Exp: 01/20 - CVV: 123
+        </div>
+
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_njs9ThSIEa0izjrD5CNtde7Z002Gu6w0e7';
+
+const onToken = token => {
+        console.log(token);
+        alert('Payment SuccessFul');
+    }
+
+
+    return (
+        <StripeCheckout 
+            label='Pay Now'
+            name='CROWN CLOTHINGS LTD'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+
+export default StripeCheckoutButton;
